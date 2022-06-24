2ad981c82bee8bbd1c638828435715f01ac5a1c5
Katharina Ulasik <katharina.ulasik@tailored-apps.com>
2022-06-24 11:24:28 +0200

    Initial Commit

---
+++ HeartPressure/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeartPressure
+//
+//  Created by Katharina on 24.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HeartPressure/HeartPressureApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartPressureApp.swift
+//  HeartPressure
+//
+//  Created by Katharina on 24.06.22.
+//
+
+import SwiftUI
+
+@main
+struct HeartPressureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
